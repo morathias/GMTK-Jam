3f64bdd415d2824415d3301a5247ba4aa43b6acc
Lucas de Hoz <dehozlucas@gmail.com>
2022-07-15 19:06:04 -0300

    removed namespaces

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,16 +1,13 @@
 ﻿using UnityEngine;
 
-namespace DefaultNamespace
+public class Player : MonoBehaviour
 {
-    public class Player : MonoBehaviour
-    {
-        public PlayerMovement playerMovement;
-        public PlayerRotation playerRotation;
+    public PlayerMovement playerMovement;
+    public PlayerRotation playerRotation;
 
-        public void Block(bool blocked)
-        {
-            playerMovement.enabled = !blocked;
-            playerRotation.enabled = !blocked;
-        }
+    public void Block(bool blocked)
+    {
+        playerMovement.enabled = !blocked;
+        playerRotation.enabled = !blocked;
     }
 }
