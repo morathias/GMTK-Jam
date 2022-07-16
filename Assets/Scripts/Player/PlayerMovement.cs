1655af2bf6e133368fed4bfac5fbacd6b7f46182
cscaliaSpiky <cscaliaspiky@gmail.com>
2022-07-16 19:02:30 -0300

    Animation Linked with Player Speed

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -8,11 +8,16 @@ public class PlayerMovement : MonoBehaviour
     public Camera camera;
     public Vector3 Direction =>  camera.transform.right * Input.GetAxisRaw("Horizontal") * speed + camera.transform.forward.ProjectUp().normalized * Input.GetAxisRaw("Vertical") * speed;
 
-    public WeaponSwitcher weaponSwitcher; 
+    public WeaponSwitcher weaponSwitcher;
+
+    public Animator animator;
+    private static readonly int Multiplier = Animator.StringToHash("Multiplier");
+
     void Update()
     {
         Vector3 direction = Direction;
         direction.y += gravity;
+        animator.SetFloat(Multiplier, weaponSwitcher.currentIWeapon.speedMultiplier);
         charController.Move(direction * Time.deltaTime * weaponSwitcher.currentIWeapon.speedMultiplier);
     }
 
