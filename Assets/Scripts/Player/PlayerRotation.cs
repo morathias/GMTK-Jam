91e0ff98686cf232eb83c1256187387f4a052873
Franco <el-pepi@live.com.ar>
2022-07-16 01:09:39 -0300

    mati rotation, made mouse look not suck

--- Assets/Scripts/Player/PlayerRotation.cs
+++ Assets/Scripts/Player/PlayerRotation.cs
@@ -4,20 +4,27 @@ public class PlayerRotation : MonoBehaviour
 {
     public float rotationSpeed = 12;
     public Camera camera;
+    public Transform view;
+    public Vector3 rotationOffset;
+
+    Plane mousePlane = new Plane();
     
     void FixedUpdate()
     {
         Vector3 mouseInWorldPos = GetMouseInWorldPoint();
         Quaternion rot = Quaternion.LookRotation((mouseInWorldPos - transform.position).ProjectUp());
         transform.rotation = Quaternion.Lerp(transform.rotation, rot, rotationSpeed * Time.fixedDeltaTime);
+        view.localRotation = Quaternion.identity;
+        view.Rotate(rotationOffset, Space.World);
     }
 
     Vector3 GetMouseInWorldPoint()
     {
+        mousePlane.SetNormalAndPosition(Vector3.up, transform.position);
         Ray ray = camera.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out RaycastHit raycast, 100))
+        if (mousePlane.Raycast(ray, out float dist))
         {
-            return raycast.point;
+            return ray.GetPoint(dist);
         }
         return transform.position + transform.forward;
     }
